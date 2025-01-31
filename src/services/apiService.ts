57c47466e0a2779c606100eae474a202aee07c1a
calebomondi <caleb.omondi@students.jkuat.ac.ke>
2025-01-31 13:10:53 +0300

    fix: update API endpoints in apiService for consistency with new routing structure

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -5,7 +5,7 @@ import API_URL from './apiUrl';
 const apiService = {
   getMyClassesToday: async (teacher_id: string): Promise<MyClassesToday[]> => {
     try {
-      const response: AxiosResponse<MyClassesToday[]> = await axios.get(`${API_URL}/myclasses-today?teacher_id=${teacher_id}`);
+      const response: AxiosResponse<MyClassesToday[]> = await axios.get(`${API_URL}/api/tutor/myclasses-today?teacher_id=${teacher_id}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -14,7 +14,7 @@ const apiService = {
   },
   geAttendanceStats: async (unit_id: string): Promise<AttendedStats[]> => {
     try {
-      const response: AxiosResponse<AttendedStats[]> = await axios.get(`${API_URL}/attendance-stats?unit_id=${unit_id}`);
+      const response: AxiosResponse<AttendedStats[]> = await axios.get(`${API_URL}/api/tutor/attendance-stats?unit_id=${unit_id}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -23,7 +23,7 @@ const apiService = {
   },
   getAttendanceSummary: async (unit_id: string): Promise<AttendedSummary[]> => {
     try {
-      const response: AxiosResponse<AttendedSummary[]> = await axios.get(`${API_URL}/attendance-summary?unit_id=${unit_id}`);
+      const response: AxiosResponse<AttendedSummary[]> = await axios.get(`${API_URL}/api/tutor/attendance-summary?unit_id=${unit_id}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -32,7 +32,7 @@ const apiService = {
   },
   getTeacherInfo: async (email: string): Promise<TeacherInfo> => {
     try {
-      const response: AxiosResponse<TeacherInfo> = await axios.get(`${API_URL}/teacher-info?email=${email}`);
+      const response: AxiosResponse<TeacherInfo> = await axios.get(`${API_URL}/api/tutor/teacher-info?email=${email}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -41,7 +41,7 @@ const apiService = {
   },
   getConfirmClass: async (unit_id:string): Promise<ConfirmClass[]> => {
     try {
-      const response: AxiosResponse<ConfirmClass[]> = await axios.get(`${API_URL}/confirmed-today?unit_id=${unit_id}`);
+      const response: AxiosResponse<ConfirmClass[]> = await axios.get(`${API_URL}/api/tutor/confirmed-today?unit_id=${unit_id}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -50,7 +50,7 @@ const apiService = {
   },
   postStartClassSession: async (unit_id:string): Promise<StartClassSession> => {
     try {
-      const response: AxiosResponse<StartClassSession> = await axios.post(`${API_URL}/start-classsession?unitId=${unit_id}`);
+      const response: AxiosResponse<StartClassSession> = await axios.post(`${API_URL}/api/tutor/start-classsession?unitId=${unit_id}`);
       console.log('postStartClassSession:', response.data);
       return response.data;
     } catch (error) {
@@ -60,7 +60,7 @@ const apiService = {
   },
   putEndClassSession: async (unit_id:string): Promise<EndClassSession> => {
     try {
-      const response: AxiosResponse<EndClassSession> = await axios.put(`${API_URL}/finish-classsession?unitId=${unit_id}`);
+      const response: AxiosResponse<EndClassSession> = await axios.put(`${API_URL}/api/tutor/finish-classsession?unitId=${unit_id}`);
       console.log('putEndClassSession:', response.data);
       return response.data;
     } catch (error) {
@@ -70,7 +70,7 @@ const apiService = {
   },
   isClassActive: async (unit_id:string): Promise<ClassIsActive> => {
     try {
-      const response: AxiosResponse<ClassIsActive> = await axios.get(`${API_URL}/active-session?unitId=${unit_id}`);
+      const response: AxiosResponse<ClassIsActive> = await axios.get(`${API_URL}/api/class/active-session?unitId=${unit_id}`);
       console.log('isClassActive:', response.data);
       return response.data;
     } catch (error) {
@@ -80,7 +80,7 @@ const apiService = {
   },
   endBeforeTime: async (unit_id:string): Promise<ClassIsActive> => {
     try {
-      const response: AxiosResponse<ClassIsActive> = await axios.get(`${API_URL}/end-before-time?unitId=${unit_id}`);
+      const response: AxiosResponse<ClassIsActive> = await axios.get(`${API_URL}/api/class/end-before-time?unitId=${unit_id}`);
       console.log('endBeforeTime:', response.data);
       return response.data;
     } catch (error) {
@@ -90,7 +90,7 @@ const apiService = {
   },
   checkSessionEnd: async (unit_id:string): Promise<ClassEndTime> => {
     try {
-      const response: AxiosResponse<ClassEndTime> = await axios.get(`${API_URL}/check-session-end?unitId=${unit_id}`);
+      const response: AxiosResponse<ClassEndTime> = await axios.get(`${API_URL}/api/class/check-session-end?unitId=${unit_id}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -99,7 +99,7 @@ const apiService = {
   },
   sessionStarted: async (unit_id:string): Promise<SessionStarted> => {
     try {
-      const response: AxiosResponse<SessionStarted> = await axios.get(`${API_URL}/session-started?unitId=${unit_id}`);
+      const response: AxiosResponse<SessionStarted> = await axios.get(`${API_URL}/api/class/session-started?unitId=${unit_id}`);
       console.error('sessionStarted-->', response.data);
       return response.data;
     } catch (error) {
@@ -109,7 +109,7 @@ const apiService = {
   },
   checkInAttendance: async (unit_id:string,student_id:string): Promise<InAttendance> => {
     try {
-      const response: AxiosResponse<InAttendance> = await axios.get(`${API_URL}/in-attendance?unitId=${unit_id}&studentId=${student_id}`);
+      const response: AxiosResponse<InAttendance> = await axios.get(`${API_URL}/api/student/in-attendance?unitId=${unit_id}&studentId=${student_id}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -118,7 +118,7 @@ const apiService = {
   },
   uploadSingleStudent: async (unit_id:string,student_id:string): Promise<UploadSingle> => {
     try {
-      const response: AxiosResponse<UploadSingle> = await axios.post(`${API_URL}/upload-single?unitId=${unit_id}&studentId=${student_id}`);
+      const response: AxiosResponse<UploadSingle> = await axios.post(`${API_URL}/api/student/upload-single?unitId=${unit_id}&studentId=${student_id}`);
       return response.data;
     } catch (error) {
       console.error('Error fetching data:', error);
